1dc15f8f633fba4408fa9f8d34c25cf276039d80
Parth Mistri <47599951+parthmistri@users.noreply.github.com>
2021-05-27 08:57:06 +0530

    Add files via upload

---
+++ day-1/task2.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h1>
+Your Name is <?php echo $_POST["uname"]; ?>.<br>
+Your Email address is <?php echo $_POST["email"];?>.<br>
+Your Mobile Number is <?php echo $_POST['phone'];?>.
+<br>
+Your Password is <?php echo $_POST["pwd"];
+?>.
+</h1>
+</body>
+</html>
