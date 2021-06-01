fef1af649675f99438c5cd362700596a254fdb19
Parth Mistri <47599951+parthmistri@users.noreply.github.com>
2021-06-01 19:25:47 +0530

    Day4

---
+++ day-4/Parth Techno/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+<meta name="keywords" content=""/>
+<meta name="description" content=""/>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Parth Techno</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+	include './menu.php';
+	include './header.php';
+        include './content.php';
+        include './sidelayout.php';
+        include './footer.php';
+?>
+   
+</body>
+</html>
---
+++ day-4/Parth Techno/sidelayout.php
@@ -0,0 +1,19 @@
+
+<div id="sidebar">
+			<ul>
+				
+				<li>
+					<h2>Languages</h2>
+					<ul>
+						<li><a href="#">PHP</a></li>
+						<li><a href="#">Python</a></li>
+						<li><a href="#">JAVA</a></li>
+						<li><a href="#">Android</a></li>
+						<li><a href="#">AngularJS</a></li>
+						<li><a href="#">NodeJS</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+        
