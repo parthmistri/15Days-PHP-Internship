303652e40a0744a3cfe87e0dbce63e8c3bc9fddf
Parth Mistri <47599951+parthmistri@users.noreply.github.com>
2021-06-04 16:56:34 +0530

    day9

---
+++ day-9/delete.php
@@ -0,0 +1,13 @@
+<?php  
+	$connection	= mysqli_connect('localhost','root','','db_internship'); 
+
+	$id	= $_GET['deleteid'];
+
+
+	$q= mysqli_query($connection, "delete from tbl_user where user_id='{$id}'") or die(mysqli_error($connection));
+
+
+	if ($q) {
+		echo "<script>alert('Record Deleted');window.location='display.php</script>";
+	}
+?>
