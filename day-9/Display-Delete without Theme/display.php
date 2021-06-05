a65180747d918895573ee92fc449ca33a39dd9ee
Parth Mistri <47599951+parthmistri@users.noreply.github.com>
2021-06-05 08:26:01 +0530

    day9

---
+++ day-9/Display-Delete without Theme/display.php
@@ -0,0 +1,42 @@
+<?php 
+
+	$connection	= mysqli_connect("localhost","root","","db_internship");
+	//Query
+	$q = mysqli_query($connection,"select * from tbl_student") or die(mysqli_error($connection));
+
+	echo "<table border='1'>";
+	echo "<tr>";
+	echo "<th>ID</th>";
+	echo "<th>Name</th>";
+	echo "<th>Gender</th>";
+	echo "<th>Date of Birth</th>";
+	echo "<th>Mobile</th>";
+	echo "<th>Address</th>";
+	echo "<th>Password</th>";
+	echo "<th>Area</th>";
+	echo "<th>Pincode</th>";
+	echo "<th>BloodGroup</th>";
+	echo "<th>Action</th>";
+	echo "</tr>";
+	while ($row =mysqli_fetch_array($q)) {
+
+		echo "<tr>";
+		echo "<td>{$row['st_id']}</td>";
+		echo "<td>{$row['st_name']}</td>";
+		echo "<td>{$row['st_gender']}</td>";
+		echo "<td>{$row['st_dob']}</td>";
+		echo "<td>{$row['st_mobile']}</td>";
+		echo "<td>{$row['st_address']}</td>";
+		echo "<td>{$row['st_password']}</td>";
+		echo "<td>{$row['st_area']}</td>";
+		echo "<td>{$row['st_pincode']}</td>";
+		echo "<td>{$row['st_bloodgroup']}</td>";
+
+
+		echo "<td><a href='delete.php?deleteid={$row['st_id']}'>Delete</a></td>";
+		echo "</tr>";
+			}
+echo "</table>";
+echo "<a href='insert.php'>Add Record</a>";
+ ?>
+
