605c300a402db39a204dbc0afd4cf7953f153f53
Parth Mistri <47599951+parthmistri@users.noreply.github.com>
2021-06-11 07:40:28 +0530

    day6

---
+++ day-6/themeintegration3-AdminPanel/navigation-bar-edit.php
@@ -0,0 +1,57 @@
+<!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="waves-effect waves-dark" href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-dark" href="<!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="waves-effect waves-dark" href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-dark" href="insert-data.php"><i class="fa fa-info"></i> Insert</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-dark" href="delete-data.php"><i class="fa fa-minus-circle"></i> Delete</a>
+                    </li>
+                    <li>
+                        <a class="active-menu waves-effect waves-dark" href="update-data.php"><i class="fa fa-edit"></i> Edit</a>
+                    </li>
+                  
+
+
+                
+                    </li>
+                   
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->insert-data.php"><i class="fa fa-info"></i> Insert</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-dark" href="delete-data.php"><i class="fa fa-minus-circle"></i> Delete</a>
+                    </li>
+                    <li>
+                        <a class="active-menu waves-effect waves-dark" href="update-data.php"><i class="fa fa-edit"></i> Edit</a>
+                    </li>
+                  
+
+
+                
+                    </li>
+                   
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
